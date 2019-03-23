7a00884ccb73b37206583e4d8bdcc079b5e00a1e
Bleck <OverlordBleck@gmail.com>
2019-03-23 17:19:37 -0500

    main derma structure

--- lua/cfc_voice_channels/core/cl/cl_init.lua
+++ lua/cfc_voice_channels/core/cl/cl_init.lua
@@ -1,2 +1,4 @@
 
 -- includes go here /shrug
+
+include("cfc_voice_channels/core/cl/dermas/cfc_voice_derma.lua")
---
+++ lua/cfc_voice_channels/core/cl/dermas/cfc_voice_derma.lua
@@ -0,0 +1,28 @@
+
+local Panel = {}
+
+local function addChannels(parent, passwordProtected, name, ownerName, id)
+
+end
+
+function Panel:Init()
+    self.Main = vgui.Create("DFrame", Panel)
+    self.Main:SetSize(100,200)
+    self.Main:MakePopup()
+
+    self.List = vgui.Create("DListView", Panel)\
+    self.List:Dock(FILL)
+    self.List:SetMultiSelect(false)
+    self.List:AddColumn("#")
+    self.List:AddColumn("Channel")
+    self.List:AddColumn("Users")
+    self.List:AddColumn("Locked")
+
+    self.List:AddLine("1", "#yeetus", "5", "true") -- TODO: actual list of channels =^)
+
+    function self.List:DoDoubleClick(lineID, line)
+        -- TODO: Open channel view
+    end
+end
+
+vgui.Register("cfc_voice_main_derma", Panel)
