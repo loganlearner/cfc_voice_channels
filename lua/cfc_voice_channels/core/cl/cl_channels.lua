f317b39fd87de6856acae20ccf218e1cd03a7299
Bleck <OverlordBleck@gmail.com>
2019-03-23 17:41:14 -0500

    concommand structures

--- lua/cfc_voice_channels/core/cl/cl_channels.lua
+++ lua/cfc_voice_channels/core/cl/cl_channels.lua
@@ -2,3 +2,20 @@ function cfc_voice:FetchChannels()
     net.Start("gimmeChannelsPls")
     net.SendToServer()
 end
+
+concommand.Add("cfc_open_voice_channels", function()
+    cfc_voice:FetchChannels()
+    vgui.Create("cfc_voice_main_derma")
+end)
+
+concommand.Add("cfc_create_voice_channel", function(ply, cmd, args)
+
+end)
+
+concommand.Add("cfc_voice_disconnect", function()
+
+end)
+
+concommand.Add("cfc_voice_connect", function(ply, cmd, args)
+
+end)
