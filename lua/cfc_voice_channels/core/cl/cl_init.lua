5d0ac8b64ad28f59b9cf531b0b6d89a4d76dc98b
Bleck <OverlordBleck@gmail.com>
2019-07-22 19:38:31 -0500

    Is muted check

--- lua/cfc_voice_channels/core/cl/cl_init.lua
+++ lua/cfc_voice_channels/core/cl/cl_init.lua
@@ -1,11 +1,10 @@
--- includes go here /shrug
-cfc_voice.connectedChannel = nil
-cfc_voice.selectedChannel = nil
-
 include( "cfc_voice_channels/core/cl/cl_channels.lua" )
 
--- Dermas
+--Dermas 
 include( "cfc_voice_channels/core/cl/dermas/cfc_voice_derma.lua" )
 include( "cfc_voice_channels/core/cl/dermas/mini/channel_list.lua" )
 include( "cfc_voice_channels/core/cl/dermas/mini/channel_view.lua" )
 include( "cfc_voice_channels/core/cl/dermas/password_derma.lua" )
+
+cfc_voice.connectedChannel = nil
+cfc_voice.selectedChannel = nil
